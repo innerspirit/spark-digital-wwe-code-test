9af66208aa8e755154d22b41077a3505227a59e0
Christian Maioli <mmchristian@gmail.com>
2018-03-30 13:31:21 -0300

    Basic home page markup and images

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,56 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <header>
+      <nav>
+        <img src="./assets/logo.png" alt="WWE Network" />
+        <ul>
+          <li>Redeem Prepaid Card</li>
+          <li>Login</li>
+        </ul>
+        <button>Join Now</button>
+      </nav>
+    </header>
+
+    <article>
+      <hr />
+      <h1>The ONLY place to watch SummerSlam, WrestleMania and thousands of hours of WWE video on demand.</h1>
+      <div class="row">
+        <div class="col-m-6">
+          <button>Join Free for a Month</button>
+          <small>No commitment, cancel anytime</small>
+        </div>
+        <div class="col-m-6">
+          <img src="./assets/summerslam.png" alt="Summer Slam">
+          <strong>LIVE</strong>
+          This Sunday 7e/4p
+        </div>
+        <img src="./assets/downarrow.png" alt="Scroll down">
+      </div>
+    </article>
+
+    <footer>
+      <div class="row">
+        <div class="col-m-4">
+          <strong>For just $9.99 per month,</strong> WWE Network includes every pay-per-view event live at no additional cost
+          </div>
+        <div class="col-m-4">
+          <strong>Unlimited access</strong> — anywhere, anytime, on any device
+        </div>
+        <div class="col-m-4">
+          <strong>First month FREE</strong> — No commitment, cancel anytime
+        </div>
+      </div>
+    </footer>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+  },
+  data: () => {
   }
 }
 </script>
