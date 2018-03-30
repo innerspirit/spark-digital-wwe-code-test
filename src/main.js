0138c4871876b08cdfd6d3a1f217b354c5d5b57e
Christian Maioli <mmchristian@gmail.com>
2018-03-30 13:30:53 -0300

    Installed Bootstrap

--- src/main.js
+++ src/main.js
@@ -1,6 +1,11 @@
 import Vue from 'vue'
+import BootstrapVue from 'bootstrap-vue/dist/bootstrap-vue.esm';
+import 'bootstrap-vue/dist/bootstrap-vue.css';
+import 'bootstrap/dist/css/bootstrap.css';
+
 import App from './App.vue'
 
+Vue.use(BootstrapVue);
 Vue.config.productionTip = false
 
 new Vue({
