aaac0d28707c191eae98e4112c6241ae5656b59e
Christian Maioli <mmchristian@gmail.com>
2018-03-30 16:55:25 -0300

    Nav and main styling

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,35 @@
 <template>
   <div id="app">
-    <header>
-      <nav>
-        <img src="./assets/logo.png" alt="WWE Network" />
-        <ul>
-          <li>Redeem Prepaid Card</li>
-          <li>Login</li>
-        </ul>
-        <button type="button" class="btn btn-danger">Join Now</button>
-      </nav>
+    <header class="container-fluid">
+      <div class="row">
+        <div class="col-md-2">
+          <img class="logo" src="./assets/logo.png" alt="WWE Network" />
+        </div>
+        <div class="col-md-4 offset-6 text-right">
+          <a href="#">Redeem Prepaid Card</a> | <a href="#">Login</a>
+          <button type="button" class="btn btn-danger">Join Now</button>
+        </div>
+      </div>
     </header>
 
-    <article>
-      <hr />
-      <h1>The ONLY place to watch SummerSlam, WrestleMania and thousands of hours of WWE video on demand.</h1>
+    <article class="container-fluid">
       <div class="row">
-        <div class="col-md-6">
-          <button type="button" class="btn btn-danger">Join Free for a Month</button>
-          <small>No commitment, cancel anytime</small>
+        <div class="col-md-8 offset-2">
+          <hr />
+          <h1>The ONLY place to watch SummerSlam, WrestleMania and thousands of hours of WWE video on demand.</h1>
+          <div class="row">
+            <div class="col-md-6">
+              <button type="button" class="btn btn-danger">Join Free for a Month</button>
+              <small>No commitment, cancel anytime</small>
+            </div>
+            <div class="col-md-6">
+              <img src="./assets/summerslam.png" alt="Summer Slam">
+              <strong>LIVE</strong>
+              This Sunday 7e/4p
+            </div>
+            <img src="./assets/downarrow.png" alt="Scroll down">
+          </div>
         </div>
-        <div class="col-md-6">
-          <img src="./assets/summerslam.png" alt="Summer Slam">
-          <strong>LIVE</strong>
-          This Sunday 7e/4p
-        </div>
-        <img src="./assets/downarrow.png" alt="Scroll down">
       </div>
     </article>
 
@@ -53,13 +58,51 @@ export default {
 }
 </script>
 
-<style>
+<style lang="scss">
 #app {
   font-family: 'Roboto', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  color: white;
+  background-color: black;
+
+  a {
+    color: white;
+
+    &hover {
+      text-decoration: underline;
+    }
+  }
+}
+
+header.container-fluid {
+  background-color: rgba(0, 0, 0, 0.5);
+  width: 1370px;
+
+  .row {
+    height: 85px;
+    padding-top: 22px;
+    align-items: center;
+  }
+
+  button {
+    margin-left: 15px;
+    padding-left: 17px;
+    padding-right: 17px;
+  }
+}
+
+article {
+  height: 800px;
+  background: top center no-repeat url('./assets/fighters.jpg');
+
+  hr {
+    margin-top: 400px;
+    background-color: red;
+    width: 750px;
+    height: 3px;
+  }
+
 }
 </style>
