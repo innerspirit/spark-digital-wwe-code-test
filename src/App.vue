654e30d53a49309d208675081936df1c3f1c2820
Christian Maioli <mmchristian@gmail.com>
2018-04-05 07:14:12 -0300

    Bugfixing

--- src/App.vue
+++ src/App.vue
@@ -3,10 +3,10 @@
     <header class="fixed-top">
       <div class="container-fluid">
         <div class="row">
-          <div class="col-md-2">
+          <div class="col-2">
             <img class="logo" src="./assets/logo.png" alt="WWE Network" />
           </div>
-          <div class="col-md-4 offset-6 text-right">
+          <div class="col-10 text-right">
             <a href="#">Redeem Prepaid Card</a> | <a href="#">Login</a>
             <button type="button" class="btn btn-danger">Join Now</button>
           </div>
@@ -16,17 +16,17 @@
 
     <article class="container-fluid">
       <div class="row">
-        <div class="col-md-8 offset-2">
+        <div class="col-md-8 offset-md-2 col-10 offset-1">
           <hr />
           <h1>The ONLY place to watch SummerSlam, WrestleMania and thousands of hours of WWE video on demand.</h1>
           <div class="row join-row">
-            <div class="col-md-5 offset-1">
+            <div class="col-md-5 offset-md-1 col-6">
               <div class="float-right left-col">
                 <button type="button" class="btn btn-danger btn-lg">Join Free for a Month</button>
                 <small>No commitment, cancel anytime</small>
               </div>
             </div>
-            <div class="col-md-6 right-col text-left">
+            <div class="col-6 right-col text-left">
               <img src="./assets/summerslam.png" alt="Summer Slam">
 
               <div class="container right-text">
@@ -44,13 +44,13 @@
 
     <footer>
       <div class="row">
-        <div class="col-md-4 footer-card">
+        <div class="col-4 footer-card">
           <strong>For just $9.99 per month,</strong> WWE Network includes every pay-per-view event live at no additional cost
         </div>
-        <div class="col-md-4 footer-card">
+        <div class="col-4 footer-card">
           <strong>Unlimited access</strong> — anywhere, anytime, on any device
         </div>
-        <div class="col-md-4 footer-card">
+        <div class="col-4 footer-card">
           <strong>First month FREE</strong> — No commitment, cancel anytime
         </div>
       </div>
@@ -89,10 +89,10 @@ body {
 }
 
 header {
-  background-color: rgba(0, 0, 0, 0.5);
+  background-color: rgb(50, 50, 50);
 
   .container-fluid {
-    width: 1370px;
+    width: 82%;
 
     .row {
       height: 85px;
@@ -116,17 +116,30 @@ article {
   hr {
     margin-top: 451px;
     background-color: red;
-    width: 772px;
+    width: 82%  ;
     height: 3px;
     margin-bottom: 36px;
   }
 
   h1 {
     font-size: 3rem;
+    max-width: 885px;
+    margin: 0px auto;
   }
 
   .join-row {
     padding-top: 20px;
+
+    > :first-child:after {
+      content: " ";
+      position: absolute;
+      border-left: 1px grey solid;
+      top: 11%;
+      right: -4px;
+      height: 65%;
+      margin-top: auto;
+      margin-bottom: auto;
+    }
 
     .left-col {
       padding: 22px 24px;
@@ -142,6 +155,7 @@ article {
         font-size: 1.2rem;
         font-weight: lighter;
         font-style: italic;
+        color: #c3b3ac;
       }
     }
 
@@ -152,6 +166,7 @@ article {
 
       .right-text {
         padding-top: 31px;
+        white-space: nowrap;
 
         strong {
           color: red;
@@ -173,21 +188,39 @@ article {
 }
 
 footer {
+  background: linear-gradient(#363e54, #564a4e);
 
   .row {
     align-items: center;
     padding: 45px 25px;
+    text-align: left;
+    display: table;
 
-    >:nth-child(n+2) {
-      border-left: 1px solid white;
+    > :not(:last-child):after {
+      content: " ";
+      position: absolute;
+      border-left: 2px white solid;
+      top: 2px;
+      right: -4px;
+      height: 99px;
+      margin-top: auto;
+      margin-bottom: auto;
     }
   }
 
   .footer-card {
-    padding: 15px 60px;
-    font-size: 1.40rem;
-    line-height: 1.3;
+    padding: 15px 44px 15px 82px;
+    font-size: 1.38em;
+    line-height: 1.15;
+    display: table-cell;
     font-weight: lighter;
+    width: 33.33333333%;
+    height: 110px;
+    vertical-align: middle;
   }
+}
+
+button.btn {
+    background-color: #ea1a29;
 }
 </style>
