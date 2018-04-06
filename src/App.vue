187e24541d62dfc4ed830c69bb19610658ec3279
Christian Maioli <mmchristian@gmail.com>
2018-04-06 18:24:15 -0300

    Responsive mobile fixes

--- src/App.vue
+++ src/App.vue
@@ -3,10 +3,10 @@
     <header class="fixed-top">
       <div class="container-fluid">
         <div class="row">
-          <div class="col-2">
-            <img class="logo" src="./assets/logo.png" alt="WWE Network" />
+          <div class="col-12 col-md-2">
+            <img class="logo w-100" src="./assets/logo.png" alt="WWE Network" />
           </div>
-          <div class="col-10 text-right">
+          <div class="col-12 col-md-auto text-right ml-auto">
             <a href="#">Redeem Prepaid Card</a> | <a href="#">Login</a>
             <button type="button" class="btn btn-danger">Join Now</button>
           </div>
@@ -20,18 +20,20 @@
           <hr />
           <h1>The ONLY place to watch SummerSlam, WrestleMania and thousands of hours of WWE video on demand.</h1>
           <div class="row join-row">
-            <div class="col-md-5 offset-md-1 col-6">
-              <div class="float-right left-col">
+            <div class="col-md-5 offset-md-1 col-12">
+              <div class="float-md-right left-col">
                 <button type="button" class="btn btn-danger btn-lg">Join Free for a Month</button>
                 <small>No commitment, cancel anytime</small>
               </div>
             </div>
-            <div class="col-6 right-col text-left">
-              <img src="./assets/summerslam.png" alt="Summer Slam">
+            <div class="col-md-6 right-col text-left col-12">
+              <div class="d-flex justify-content-center justify-content-md-start">
+                <img class="col-6 col-md-auto align-self-center" src="./assets/summerslam.png" alt="Summer Slam">
 
-              <div class="container right-text">
-                <div class="row"><strong>LIVE</strong></div>
-                <div class="row">This Sunday<br />7e/4p</div>
+                <div class="col-auto right-text">
+                  <strong>LIVE</strong><br />
+                  This Sunday<br />7e/4p
+                </div>
               </div>
             </div>
             <div class="container down-arrow">
@@ -44,13 +46,13 @@
 
     <footer>
       <div class="row">
-        <div class="col-4 footer-card">
+        <div class="col-md-4 col-12 footer-card">
           <strong>For just $9.99 per month,</strong> WWE Network includes every pay-per-view event live at no additional cost
         </div>
-        <div class="col-4 footer-card">
+        <div class="col-md-4 col-12 footer-card">
           <strong>Unlimited access</strong> — anywhere, anytime, on any device
         </div>
-        <div class="col-4 footer-card">
+        <div class="col-md-4 col-12 footer-card">
           <strong>First month FREE</strong> — No commitment, cancel anytime
         </div>
       </div>
@@ -92,12 +94,18 @@ header {
   background-color: rgb(50, 50, 50);
 
   .container-fluid {
-    width: 82%;
+    @media (min-width: 768px) {
+      width: 82%;
+    }
 
     .row {
-      height: 85px;
       padding-top: 22px;
       align-items: center;
+      padding-bottom: 15px;
+
+      @media (min-width: 768px) {
+        height: 85px;
+      }
     }
 
     button {
@@ -114,11 +122,15 @@ article {
   background: top center no-repeat url('./assets/fighters.jpg');
 
   hr {
-    margin-top: 451px;
     background-color: red;
-    width: 82%  ;
+    width: 82%;
     height: 3px;
     margin-bottom: 36px;
+    margin-top: 151px;
+
+    @media (min-width: 768px) {
+      margin-top: 451px;
+    }
   }
 
   h1 {
@@ -130,25 +142,33 @@ article {
   .join-row {
     padding-top: 20px;
 
-    > :first-child:after {
-      content: " ";
-      position: absolute;
-      border-left: 1px grey solid;
-      top: 11%;
-      right: -4px;
-      height: 65%;
-      margin-top: auto;
-      margin-bottom: auto;
+    @media (min-width: 768px) {
+      > :first-child:after {
+        content: " ";
+        position: absolute;
+        border-left: 1px grey solid;
+        top: 11%;
+        right: -4px;
+        height: 65%;
+        margin-top: auto;
+        margin-bottom: auto;
+      }
     }
 
     .left-col {
-      padding: 22px 24px;
+      @media (min-width: 768px) {
+        padding: 22px 24px;
+      }
 
       button.btn-lg {
         display: block;
         font-size: 1.7rem;
         padding: 19px 26px;
         margin-bottom: 10px;
+
+        @media (max-width: 768px) {
+          margin: 0px auto;
+        }
       }
 
       small {
@@ -163,6 +183,10 @@ article {
       padding-left: 37px;
       font-size: 1.55rem;
       line-height: 1.2;
+
+      @media (max-width: 768px) {
+        margin: 0px auto;
+      }
 
       .right-text {
         padding-top: 31px;
@@ -182,8 +206,12 @@ article {
   }
 
   .down-arrow {
-    margin-top: 80px;
     align-items: center;
+    margin-top: 20px;
+
+    @media (min-width: 768px) {
+      margin-top: 80px;
+    }
   }
 }
 
@@ -194,17 +222,22 @@ footer {
     align-items: center;
     padding: 45px 25px;
     text-align: left;
-    display: table;
 
-    > :not(:last-child):after {
-      content: " ";
-      position: absolute;
-      border-left: 2px white solid;
-      top: 2px;
-      right: -4px;
-      height: 99px;
-      margin-top: auto;
-      margin-bottom: auto;
+    @media (min-width: 768px) {
+      display: table;
+      width: 82%;
+      margin: 0px auto;
+
+      > :not(:last-child):after {
+        content: " ";
+        position: absolute;
+        border-left: 2px white solid;
+        top: 2px;
+        right: -4px;
+        height: 99px;
+        margin-top: auto;
+        margin-bottom: auto;
+      }
     }
   }
 
@@ -212,11 +245,14 @@ footer {
     padding: 15px 44px 15px 82px;
     font-size: 1.38em;
     line-height: 1.15;
-    display: table-cell;
     font-weight: lighter;
-    width: 33.33333333%;
-    height: 110px;
     vertical-align: middle;
+
+    @media (min-width: 768px) {
+      height: 110px;
+      display: table-cell;
+      width: 33.33333333%;
+    }
   }
 }
 
